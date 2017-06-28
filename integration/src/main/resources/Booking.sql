ae67f3cb9fd3f6328906862438d2cfddc3b7ba24
fayaz <fayaz.sanaulla@gmail.com>
2017-06-28 15:28:13 +0200

    booking sql refactoring

--- integration/src/main/resources/Booking.sql
+++ integration/src/main/resources/Booking.sql
@@ -1,43 +1,105 @@
-//Booking
-select *
-from BookRequest as br, SalesChannel as s, Brand as b,Trade as t
-LEFT JOIN QueryProxyRequest q
-on br.query_uuid==q.query_uuid
-where br.trade_id == t.trade_id and br.brand_id==b.brand_id and br.sales_channel_id=s.sales_channel_id
+-- BOOKING
+SELECT br.search_query_uuid AS query_uuid,
+	brand_name,
+	trade_name,
+	trade_group,
+	trade_parent_group,
+	sales_channel,
+			 (unix_timestamp(end_utc_timestamp) - unix_timestamp(start_utc_timestamp)) * 1000 AS response_time_ms,
+	error_stack_trace,
+	success,
+	xml_booking_login,
+			 window(start_utc_timestamp, '5 minutes').end AS time
+FROM BookRequest AS br,
+	SalesChannel AS sc,
+	Trade AS t,
+	Brand AS b
+	LEFT JOIN QueryProxyRequest AS qpr
+		ON br.search_query_uuid == qpr.query_uuid
+WHERE br.sales_channel_id == sc.sales_channel_id
+AND br.trade_id == t.trade_id
+AND br.brand_id == b.brand_id
 
 
-//count
-select '11:35' as 'timestamp', COUNT(query_uuid) AS 'booking_count', brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login
-from Booking
-group by
-brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login
-
-//success
-select '11:35' as 'timestamp', COUNT(query_uuid) AS 'booking_success', brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login
-from Booking
-where success is not null
-group by
-brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login
-
-//error
-select '11:35' as 'timestamp', COUNT(query_uuid) AS 'booking_error', brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login
-from Booking
-where errorStackTrace is not null
-group by
-brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login
-
-
-
-// BOOKING RESPONSE TIME
-val influxPreBookingResponseTime = spark.sql("
-SELECT	brand_name,
-	sales_channel_name,
+-- BOOKING COUNT
+SELECT COUNT(query_uuid) AS booking_count,
+	time,
+	brand_name,
+	sales_channel,
 	trade_group,
 	trade_name,
-	trade_parent_name,
-	MIN(DATEDIFF(end_utc_timestamp - start_utc_timestamp)) AS 'min_response_time',
-	MAX(DATEDIFF(end_utc_timestamp - start_utc_timestamp)) AS 'max_response_time,
-	ANG(DATEDIFF(end_utc_timestamp, start_utc_timestamp)) AS 'avg_response_time
-FROM Booking
-GROUP BY brand_name, sales_channel_name, trade_group, trade_name, trade_parent_name, xml_booking_login")
-.withColumn("time", time)
+	trade_parent_group,
+	xmL_booking_login
+FROM BookingEnriched
+GROUP BY
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xml_booking_login
+
+-- BOOKING SUCCESS
+SELECT COUNT(query_uuid) AS booking_success,
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xmL_booking_login
+FROM BookingEnriched
+WHERE success IS NOT NULL
+GROUP BY
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xml_booking_login
+
+-- BOOKING FAILURE
+SELECT COUNT(query_uuid) as booking_errors,
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xmL_booking_login
+FROM BookingEnriched
+WHERE error_stack_trace IS NOT NULL
+GROUP BY
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xml_booking_login
+
+
+
+-- BOOKING RESPONSE TIME
+SELECT COUNT(query_uuid) AS booking_errors,
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xmL_booking_login,
+	min(response_time_ms) AS min_response_time_ms,
+  max(response_time_ms) AS max_response_time_ms,
+	avg(response_time_ms) AS avg_response_time_ms
+FROM BookingEnriched
+GROUP BY
+	time,
+	brand_name,
+	sales_channel,
+	trade_group,
+	trade_name,
+	trade_parent_group,
+	xml_booking_login
