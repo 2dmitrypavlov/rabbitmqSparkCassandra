05a704c3131efd34173f10a22fb92698e37d4f8f
dmitry <2dmitrypavlov@gmail.com>
2017-07-05 14:19:33 +0300

    added xmllogin

--- integration/src/main/scala/com/jactravel/monitoring/streaming/ProcessBusiness.scala
+++ integration/src/main/scala/com/jactravel/monitoring/streaming/ProcessBusiness.scala
@@ -27,7 +27,7 @@ object ProcessBusiness extends LazyLogging with ConfigService with ProcessMonito
 
 
     ///use get or create to use check point
-    ssc = new StreamingContext(spark.sparkContext, Seconds(4))
+    ssc = new StreamingContext(spark.sparkContext, Seconds(20))
     //Milliseconds(50))
     val numPar = 150
 
