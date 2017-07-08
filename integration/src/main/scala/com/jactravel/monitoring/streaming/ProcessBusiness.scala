9f81356a929861e3190fabeee916f2b8744c6011
fayaz <fayaz.sanaulla@gmail.com>
2017-07-08 22:07:22 +0200

    stream fix, test fixed

--- integration/src/main/scala/com/jactravel/monitoring/streaming/ProcessBusiness.scala
+++ integration/src/main/scala/com/jactravel/monitoring/streaming/ProcessBusiness.scala
@@ -286,7 +286,7 @@ object ProcessBusiness extends LazyLogging with ConfigService with ProcessMonito
         , l.requestURL
         , l.errorStackTrace
       ))
-    }.saveToCassandra(keyspaceName, "cmi_request_second")
+    }.saveToCassandra(keyspaceName, "cmi_request_updated_second")
 
 
     cmiBatchRequestStream.transform { rdd =>
@@ -308,7 +308,7 @@ object ProcessBusiness extends LazyLogging with ConfigService with ProcessMonito
           , l.errorStackTrace)
       }
 
-    }.repartition(numPar).saveToCassandra(keyspaceName, "cmi_batch_request")
+    }.repartition(numPar).saveToCassandra(keyspaceName, "cmi_batch_request_updated_second")
 
 
     ssc.start()
