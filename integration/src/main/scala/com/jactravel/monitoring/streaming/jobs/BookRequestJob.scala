a893fbdb3f7ad57d8db82ae30060d35014dbe036
dmitry <2dmitrypavlov@gmail.com>
2017-07-10 00:08:15 +0300

    conf changed

--- integration/src/main/scala/com/jactravel/monitoring/streaming/jobs/BookRequestJob.scala
+++ integration/src/main/scala/com/jactravel/monitoring/streaming/jobs/BookRequestJob.scala
@@ -1,68 +1,22 @@
 package com.jactravel.monitoring.streaming.jobs
 
+import com.jactravel.monitoring.model.jobs.BookRequestJobInfo
 import com.jactravel.monitoring.model.jobs.CaseClassed._
 import com.jactravel.monitoring.streaming.ConfigService
 import com.paulgoldbaum.influxdbclient._
 
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
   * Created by fayaz on 09.07.17.
   */
-object BookRequestJob extends ConfigService  {
+object BookRequestJob extends ConfigService with BookRequestJobInfo {
 
-  def toBookCountPoint(brc: BookRequestCount): Point = {
-    Point("book_request_count")
-      .addTag("mtime", brc.time)
-      .addTag("brand_name", brc.brand_name)
-      .addTag("sales_channel", brc.sales_channel)
-      .addTag("trade_group", brc.trade_group)
-      .addTag("trade_name", brc.trade_name)
-      .addTag("trade_parent_group", brc.trade_parent_group)
-      .addTag("xml_booking_login", brc.xml_booking_login)
-      .addField("book_count", brc.book_count)
-  }
 
-  def toSuccessCountPoint(brsc: BookRequestSuccessCount): Point = {
-    Point("book_success_count")
-      .addTag("mtime", brsc.time)
-      .addTag("brand_name", brsc.brand_name)
-      .addTag("sales_channel", brsc.sales_channel)
-      .addTag("trade_group", brsc.trade_group)
-      .addTag("trade_name", brsc.trade_name)
-      .addTag("trade_parent_group", brsc.trade_parent_group)
-      .addTag("xml_booking_login", brsc.xml_booking_login)
-      .addField("success_count", brsc.success_count)
-  }
-
-  def toErrorsCountPoint(brec: BookRequestErrorsCount): Point = {
-    Point("book_errors_count")
-      .addTag("mtime", brec.time)
-      .addTag("brand_name", brec.brand_name)
-      .addTag("sales_channel", brec.sales_channel)
-      .addTag("trade_group", brec.trade_group)
-      .addTag("trade_name", brec.trade_name)
-      .addTag("trade_parent_group", brec.trade_parent_group)
-      .addTag("xml_booking_login", brec.xml_booking_login)
-      .addField("errors_count", brec.errors_count)
-  }
-
-  def toResponseTimePoint(brrt: BookRequestResponseTime): Point = {
-    Point("book_response_time")
-      .addTag("mtime", brrt.time)
-      .addTag("brand_name", brrt.brand_name)
-      .addTag("sales_channel", brrt.sales_channel)
-      .addTag("trade_group", brrt.trade_group)
-      .addTag("trade_name", brrt.trade_name)
-      .addTag("trade_parent_group", brrt.trade_parent_group)
-      .addTag("xml_booking_login", brrt.xml_booking_login)
-      .addField("min_response_time", brrt.min_response_time_ms)
-      .addField("max_response_time", brrt.max_response_time_ms)
-      .addField("perc_response_time", brrt.perc_response_time_ms)
-  }
   def main(args: Array[String]): Unit = {
 
-    val nullFilter = Seq("time","brand_name", "sales_channel", "trade_parent_group", "trade_name", "trade_group", "xml_booking_login")
+    val nullFilter = Seq("time", "brand_name", "sales_channel", "trade_parent_group", "trade_name", "trade_group", "xml_booking_login")
 
     import spark.implicits._
 
@@ -118,7 +72,7 @@ object BookRequestJob extends ConfigService  {
 
     // RICH BOOK REQUEST
     spark.sql(
-          """SELECT
+      """SELECT
                 br.query_uuid AS query_uuid,
                 brand_name,
                 trade_name,
@@ -145,7 +99,7 @@ object BookRequestJob extends ConfigService  {
 
     // BOOK COUNT
     val bookCount = spark.sql(
-            """SELECT COUNT(query_uuid) as book_count,
+      """SELECT COUNT(query_uuid) as book_count,
                   time,
                   brand_name,
                   sales_channel,
@@ -162,12 +116,12 @@ object BookRequestJob extends ConfigService  {
                   trade_name,
                   trade_parent_group,
                   xml_booking_login"""
-    ).na.fill("stub", nullFilter:+"book_count")
-     .as[BookRequestCount]
+    ).na.fill("stub", nullFilter :+ "book_count")
+      .as[BookRequestCount]
 
     // BOOK SUCCESS
     val bookSuccess = spark.sql(
-           """SELECT COUNT(query_uuid) as success_count,
+      """SELECT COUNT(query_uuid) as success_count,
                   time,
                   brand_name,
                   sales_channel,
@@ -185,12 +139,12 @@ object BookRequestJob extends ConfigService  {
                   trade_name,
                   trade_parent_group,
                   xml_booking_logiN"""
-    ).na.fill("stub", nullFilter:+"success_count")
-     .as[BookRequestSuccessCount]
+    ).na.fill("stub", nullFilter :+ "success_count")
+      .as[BookRequestSuccessCount]
 
     // BOOK ERROR
     val bookError = spark.sql(
-           """SELECT COUNT(query_uuid) as errors_count,
+      """SELECT COUNT(query_uuid) as errors_count,
                   time,
                   brand_name,
                   sales_channel,
@@ -208,12 +162,12 @@ object BookRequestJob extends ConfigService  {
                   trade_name,
                   trade_parent_group,
                   xml_booking_login"""
-    ).na.fill("stub", nullFilter:+"errors_count")
-     .as[BookRequestErrorsCount]
+    ).na.fill("stub", nullFilter :+ "errors_count")
+      .as[BookRequestErrorsCount]
 
     // BOOK RESPONSE TIME
     val bookResponseTime = spark.sql(
-           """SELECT time,
+      """SELECT time,
                   brand_name,
                   sales_channel,
                   trade_group,
@@ -232,24 +186,24 @@ object BookRequestJob extends ConfigService  {
                   trade_name,
                   trade_parent_group,
                   xml_booking_login"""
-    ).na.fill("stub", nullFilter:+"min_response_time_ms":+"max_response_time_ms":+"perc_response_time_ms")
-     .as[BookRequestResponseTime]
+    ).na.fill("stub", nullFilter :+ "min_response_time_ms" :+ "max_response_time_ms" :+ "perc_response_time_ms")
+      .as[BookRequestResponseTime]
 
     // SAVING TO INFLUXDB
 
     // SAVING BOOK COUNT TO INFLUXDB
-//    bookCount.foreachPartition { partition =>
-//
-//      // Open connection to Influxdb
-//      val db = InfluxDB.connect(influxHost, influxPort).selectDatabase(influxDBname)
-//
-//      partition
-//        .map(toBookCountPoint)
-//        .foreach(p => Await.result(db.write(p), influxTimeout))
-//
-//      // Close connection
-//      db.close()
-//    }
+    bookCount.foreachPartition { partition =>
+
+      // Open connection to Influxdb
+      val db = InfluxDB.connect(influxHost, influxPort).selectDatabase(influxDBname)
+
+      partition
+        .map(toBookCountPoint)
+        .foreach(p => Await.result(db.write(p), influxTimeout))
+
+      // Close connection
+      db.close()
+    }
 
     // SAVING BOOK SUCCESS TO INFLUXDB
     bookSuccess.foreachPartition { partition =>
